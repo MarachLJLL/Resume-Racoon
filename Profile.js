c8b53f99efea450077cfd19a05f3f093a8877c70
Luis Joseph Limgenco <marachljll@gmail.com>
2025-01-25 15:57:29 -0500

    autofills all text boxes

---
+++ Profile.js
@@ -0,0 +1,39 @@
+class Profile {
+    constructor() {
+        // You can initialize these to empty strings or sample data
+        this.email = "test@example.com";
+        this.firstName = "John";
+        this.lastName = "Doe";
+        this.fullName = "John Doe";
+
+        this.phoneNumber = "555-123-4567";
+        this.streetAndNumber = "123 Main St";
+        this.address = "Apt 4B";
+        this.city = "Sample City";
+        this.province = "Sample Province";
+        this.postalCode = "12345";  
+
+        this.linkedinProfile = "https://www.linkedin.com/in/johndoe";
+        this.githubProfile = "https://github.com/johndoe";
+        this.website = "https://johndoe.com";
+        this.portfolio = "https://johndoe.com/portfolio";
+
+        this.universityName = "Example University";
+        this.expectedGraduation = "2026";
+        this.education = "Bachelor's in Computer Science";
+
+        this.diversity = "";
+        this.gender = "";
+        this.race = "";
+        this.accessibility = "";
+        this.veteran = "";
+        this.legal = "";
+        this.authorizedToWork = "Yes";
+        this.nonCompeteDisclosure = "No";
+        this.experience = "2 years of programming";
+        this.projects = "Project A, Project B";
+        this.work = "Software Engineer";
+    }
+}
+
+p = new Profile()
