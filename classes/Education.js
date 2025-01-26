5c087b0214781a375ec3966d3129287f7fa39095
Luis Joseph Limgenco <marachljll@gmail.com>
2025-01-25 23:53:30 -0500

    moving to burnside

---
+++ classes/Education.js
@@ -0,0 +1,12 @@
+class Education {
+    constructor() {
+        this.university;
+        this.degree; // This should be a dropdown
+        this.fieldOfStudy;
+        this.gpa;
+        this.startDate;
+        this.endDate;
+    }
+}
+
+console.log('loaded education')
